f450f8c15ebae45e06756488ecf9450472b1b4dd
Jeffrey Bush <jbush@moravian.edu>
2019-07-26 15:54:12 -0400

    Adding the WA method (finally)

---
+++ hist/exact/__wa.pyx
@@ -0,0 +1,282 @@
+# distutils: language=c++
+# distutils: extra_compile_args=-O3
+# distutils: define_macros=NPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
+# cython: language_level=3, boundscheck=False, wraparound=False, cdivision=True
+"""
+Cython and C++ accelerated WA filter code for doing a fancy sort with a custom comparator. See the
+wa.py file for more information.
+"""
+
+from libc.stdlib cimport malloc, free
+from libc.stdint cimport intptr_t, uint8_t
+
+from libcpp cimport bool
+from libcpp.algorithm cimport sort
+
+from libcpp.unordered_map cimport unordered_map
+from libcpp.unordered_set cimport unordered_set
+
+from numpy cimport PyArray_DATA, PyArray_CHKFLAGS, PyArray_TYPE
+from numpy cimport NPY_ARRAY_C_CONTIGUOUS, NPY_ARRAY_ALIGNED, NPY_UINT8, NPY_FLOAT64
+
+# C++ code to use with a std::sort to indirect sort the wavelet data.
+cdef extern from *:
+    r"""
+#include <type_traits>
+
+#define likely(x)   __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+typedef double dbl __attribute__((aligned()));
+typedef dbl* __restrict dbls __attribute__((aligned()));
+
+typedef intptr_t __restrict intp __attribute__((aligned()));
+typedef intp* __restrict intps __attribute__((aligned()));
+
+typedef uint8_t __restrict byte __attribute__((aligned()));
+typedef byte* __restrict bytes __attribute__((aligned()));
+
+typedef std::unordered_map<intptr_t, std::unordered_set<intptr_t>> uv_map;
+
+template <int NF=0, bool FAIL_COUNT=false>
+class CompN {
+    const int nlvls, nfltrs;
+    const dbls im; // size                              x_u => im[u]
+    intps* coords; // nlvls x size (pre-dec)            u* = coords[j][u]
+    dbls* thetas;  // nlvls x (size (post-dec))*nfltrs  |theta_uij| => thetas[j][u*,i]
+    bytes* tw0s;   // nlvls x (size (pre-dec))*nfltrs   theta_uij*w_uij_u>0 => tw0s[j][u,i]
+
+    // This map is only available if we are counting failures
+    typedef typename std::conditional<FAIL_COUNT, uv_map&, unsigned char>::type uv_map_t;
+    volatile uv_map_t fails;
+
+    template<bool _FC = FAIL_COUNT>
+    inline typename std::enable_if<_FC>::type insert_fail(intp u, intp v) const {
+        fails[u].insert(v);
+    }
+    template<bool _FC = FAIL_COUNT>
+    inline typename std::enable_if<!_FC>::type insert_fail(intp u, intp v) const {}
+
+public:
+    inline CompN(int nlvls, dbls im, intps* coords, dbls* thetas, bytes* tw0s) :
+        nlvls(nlvls), nfltrs(NF), im(im), coords(coords), thetas(thetas), tw0s(tw0s)
+    { assert(NF != 0); }
+
+    inline CompN(int nlvls, int nfltrs, dbls im, intps* coords, dbls* thetas, bytes* tw0s) :
+        nlvls(nlvls), nfltrs(nfltrs), im(im), coords(coords), thetas(thetas), tw0s(tw0s)
+    { assert(NF == 0 || NF == nfltrs); }
+
+    inline CompN(int nlvls, dbls im, intps* coords, dbls* thetas, bytes* tw0s, uv_map_t fails) :
+        nlvls(nlvls), nfltrs(NF), im(im), coords(coords), thetas(thetas), tw0s(tw0s), fails(fails)
+    { assert(NF != 0); }
+
+    inline CompN(int nlvls, int nfltrs, dbls im, intps* coords, dbls* thetas, bytes* tw0s, uv_map_t fails) :
+        nlvls(nlvls), nfltrs(nfltrs), im(im), coords(coords), thetas(thetas), tw0s(tw0s), fails(fails)
+    { assert(NF == 0 || NF == nfltrs); }
+
+    /*
+     Called to compare pixel u and v. Returns true if u < v. Returns false if u > v or if they are
+     not comparable. This really should be sorted as a poset using the algorithms in
+     https://arxiv.org/abs/0707.1532 but that is way more complicated.
+     */
+    inline bool operator()(intp u, intp v) const {
+        // Simple case - different original pixel values
+        if (likely(im[u] != im[v])) { return im[u] < im[v]; }
+
+        // Otherwise go through all sorted coefficients and calculate order
+        int nfilters = NF ? NF : this->nfltrs; // this is solved at compile-time
+        intp u_orig = u, v_orig = v;
+        for (int j = 0; j < this->nlvls; j++) { // j is the filter level
+            // Get the decimated and filter coordinates for the pixel at this level
+            intp u_dec = this->coords[j][u], v_dec = this->coords[j][v];
+            if (unlikely(u_dec == v_dec)) {
+                // Not directly comparable, but if we assume that there is some |theta_vij| smaller
+                // than both than all we have to do is find the first theta_uij*w_uij_u>0 that
+                // aren't equal between u and v and use them to sort the values.
+                bytes tw0_u = &tw0s[j][u*nfilters];
+                bytes tw0_v = &tw0s[j][v*nfilters];
+                for (int i = 0; i < nfilters; ++i) {
+                    byte tw0_u_i = tw0_u[i], tw0_v_i = tw0_v[i];
+                    if (tw0_u_i != tw0_v_i) { return tw0_u_i < tw0_v_i; }
+                }
+                // The above works as long as the w_uij_u used to distinguish the two is lined up
+                // with a theta_uij that isn't 0. If it is then we can't find a solution still (this
+                // also is because our assumption that there is a |theta_vij| smaller isn't correct
+                // since there can't be any smaller values than 0).
+                // No additional levels will help distinguish these since they will have the same u
+                // and v at any further levels.
+                insert_fail(u_orig, v_orig);
+                return false;
+            }
+
+            // The |theta_uij| and |theta_vij| values for all i
+            dbls theta_u = &thetas[j][u_dec*nfilters];
+            dbls theta_v = &thetas[j][v_dec*nfilters];
+            for (int i = 0; i < nfilters; ++i) {
+                // Check for ordering
+                dbl theta_u_i = theta_u[i], theta_v_i = theta_v[i];
+                if (theta_u_i > theta_v_i) { return !tw0s[j][u*nfilters + i]; }
+                if (theta_u_i < theta_v_i) { return  tw0s[j][v*nfilters + i]; }
+            }
+
+            // Update the coordinates
+            u = u_dec;
+            v = v_dec;
+        }
+
+        // Not enough data to make a decision (more levels could help distinguish these)
+        insert_fail(u_orig, v_orig);
+        return false;
+    }
+};
+
+typedef CompN<0, false> Comp;
+typedef CompN<4, false> Comp4;
+typedef CompN<8, false> Comp8;
+typedef CompN<0, true> CompFC;
+typedef CompN<4, true> Comp4FC;
+typedef CompN<8, true> Comp8FC;
+"""
+    # Basic types defined in the code above
+    ctypedef double dbl
+    ctypedef dbl* dbls
+    ctypedef intptr_t intp
+    ctypedef intp* intps
+    ctypedef uint8_t byte
+    ctypedef byte* bytes
+    ctypedef unordered_map[intptr_t, unordered_set[intptr_t]] uv_map
+
+    # These don't count failures, the 4 and 8 one are optimized for 2D and 3D images
+    cdef cppclass Comp:
+        Comp(int nlvls, int nfltrs, dbls im, intps* coords, dbls* thetas, bytes* tw0s) nogil
+    cdef cppclass Comp4:
+        Comp4(int nlvls, dbls im, intps* coords, dbls* thetas, bytes* tw0s) nogil
+    cdef cppclass Comp8:
+        Comp8(int nlvls, dbls im, intps* coords, dbls* thetas, bytes* tw0s) nogil
+
+    # These count failures, the 4 and 8 one are optimized for 2D and 3D images
+    cdef cppclass CompFC:
+        CompFC(int nlvls, int nfltrs, dbls im, intps* coords, dbls* thetas, bytes* tw0s, uv_map& fails) nogil
+    cdef cppclass Comp4FC:
+        Comp4FC(int nlvls, dbls im, intps* coords, dbls* thetas, bytes* tw0s, uv_map& fails) nogil
+    cdef cppclass Comp8FC:
+        Comp8FC(int nlvls, dbls im, intps* coords, dbls* thetas, bytes* tw0s, uv_map& fails) nogil
+
+cdef get_coords(shape):
+    """
+    Gets the coordinates for the decimated pixels. These allow for quick lookup using the actual
+    pixel linear index to get the linear index into the coeffs or filters arrays.
+    """
+    from functools import reduce
+    from numpy import ogrid
+    coords = ogrid[tuple(slice(x) for x in shape)]
+    return reduce(lambda x, y: x*(y.size//2) + y, (x>>1 for x in coords)).ravel()
+
+cdef inline check_array(name, arr, int dtype, shape):
+    """Checks that an array has the given data type, shape, and is C contiguous and aligned."""
+    if not (PyArray_TYPE(arr) == dtype and arr.shape == shape and
+            PyArray_CHKFLAGS(arr, NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED)):
+        raise ValueError('bad %s: expected %r of %s but got %r of %s'%
+            (name, shape, 'float64' if dtype == NPY_FLOAT64 else 'uint8', arr.shape, arr.dtype))
+
+cdef inline intptr_t count_fails(intptr_t[::1] idx, dict fails2) except -1:
+    """
+    Counts the number of failures to sort in the given set of sorted indices and a dictionary of
+    pairs that failed to sort (dict of set).
+    """
+    cdef intptr_t counter = 0, i
+    for i in range(idx.shape[0]-1):
+        if idx[i] in fails2 and idx[i+1] in fails2[idx[i]]:
+            counter += 1
+    return counter
+
+def argsort(im_, list thetas_, list tw0s_, bool return_fails=False):
+    """
+    Runs the WA argsort on the data returning the indices used to sort the data. This implements
+    steps 3 and 4 from Table I algorithm from [1]. Most of this function is actually just checking
+    arguments and converting them for use with C++. The actual sort is performed with C++ std::sort
+    with a custom comparator written in C++ above.
+
+    This function is optimized for 2D and 3D images but works for any dimension.
+
+    im     - original image, array of doubles
+    thetas - list of the absolute value of wavelet coefficients for each pixel tw0_ already in
+             sorted ordered for each pixel from largest to smallest, each element in the list is a
+             level of the wavelet
+    tw0s   - list of the uint8 (bool) values for each pixel with theta_uij * w_uij_u > 0, they are
+             ordered to line up with theta, each element in the list is a level of the wavelet
+
+    All arrays must be C-contiguous and aligned.
+
+    REFERENCES
+      1. Wan Y and Shi D, 2007, "Joint exact histogram specification and image enhancement through
+         the wavelet transform", IEEE Transcations on Image Processing, 16(9):2245-2250.
+    """
+    from numpy import arange
+    cdef int nlvls = len(thetas_), nfltrs = 1 << im_.ndim, i
+    cdef intptr_t size = im_.size
+    cdef list shapes = [im_.shape]
+    for i in range(nlvls):
+        shapes.append(tuple((x+1) // 2 for x in shapes[i]))
+
+    # Checks
+    assert nlvls == len(tw0s_), 'thetas and tw0s must have the same length'
+    check_array('im', im_, NPY_FLOAT64, im_.shape)
+    for i in range(nlvls):
+        check_array('thetas[%d]'%i, thetas_[i], NPY_FLOAT64, shapes[i+1] + (nfltrs,))
+        check_array('tw0s[%d]'%i, tw0s_[i], NPY_UINT8, shapes[i] + (nfltrs,))
+
+    # Get the linear indices and the coordinate lookups
+    cdef intptr_t[::1] idx = arange(size)
+    cdef list coords_ = [get_coords(shape) for shape in shapes[:nlvls]]
+
+    # Allocate memory for the levels of data
+    cdef intps* coords = <intptr_t**>malloc(nlvls*sizeof(intptr_t*))
+    cdef dbls* thetas = <double**>malloc(nlvls*sizeof(double*))
+    cdef bytes* tw0s = <uint8_t**>malloc(nlvls*sizeof(uint8_t*))
+    if coords is NULL or thetas is NULL or tw0s is NULL:
+        free(coords)
+        free(thetas)
+        free(tw0s)
+        raise MemoryError()
+
+    # Get data pointers
+    cdef double* im = <dbls>PyArray_DATA(im_)
+    for i in range(nlvls):
+        coords[i] = <intps>PyArray_DATA(coords_[i])
+        thetas[i] = <dbls>PyArray_DATA(thetas_[i])
+        tw0s[i] = <bytes>PyArray_DATA(tw0s_[i])
+
+    # Perform the sort
+    cdef uv_map fails
+    with nogil:
+        if return_fails:
+            if nfltrs == 4:
+                sort(&idx[0], &idx[size], Comp4FC(nlvls, im, coords, thetas, tw0s, fails))
+            elif nfltrs == 8:
+                sort(&idx[0], &idx[size], Comp8FC(nlvls, im, coords, thetas, tw0s, fails))
+            else:
+                sort(&idx[0], &idx[size], CompFC(nlvls, nfltrs, im, coords, thetas, tw0s, fails))
+        else:
+            if nfltrs == 4:
+                sort(&idx[0], &idx[size], Comp4(nlvls, im, coords, thetas, tw0s))
+            elif nfltrs == 8:
+                sort(&idx[0], &idx[size], Comp8(nlvls, im, coords, thetas, tw0s))
+            else:
+                sort(&idx[0], &idx[size], Comp(nlvls, nfltrs, im, coords, thetas, tw0s))
+
+    # Free used memory
+    free(coords)
+    free(thetas)
+    free(tw0s)
+
+    # Return the argsort data
+    if not return_fails: return idx.base
+
+    # Compute number of failures
+    cdef dict fails_ = fails, fails2
+    fails2 = fails_.copy() # create a bi-directional lookup to speed up the lookups
+    for k, v in fails_.items():
+        for x in v:
+            fails2.setdefault(x, set()).add(k)
+    return idx.base, count_fails(idx, fails2)
---
+++ hist/exact/wa.py
@@ -0,0 +1,136 @@
+"""
+Implements wavelet approach strict ordering for use with exact histogram equalization.
+"""
+
+from itertools import product
+
+def calc_info(im, nlevels=2, wavelet='haar', return_fails=False):
+    """
+    Perform exact histogram equalization using the wavelet approach as defined by [1]. Unlike the
+    other exact histogram equalization methods this returns already sorted indices as a 1D array
+    meaning the sorting step in histeq_exact is skipped. If return_fails=True then this also returns
+    the number of sorting failures (since that information is no longer available with pre-sorted
+    indices.
+
+    This calculates the extra information using the tradition (decimating) wavelet transform and
+    then uses complex sorting rules to determine the sorted order of the pixels. Even though this
+    method is referenced in many papers and used as a baseline to demonstrate improvements, it is
+    unlikely to have been implemented correctly in any of them due to the complex sorting required.
+    In some papers this is obvious by the way they reference the data (for example saying that k=9
+    and a standard strict ordering is performed).
+
+    Due to the complexity of this method and the various confusing parts of the original paper, this
+    is a best effort to do it correctly but may still be incorrect.
+
+    This implementation supports 3D data, however only isotropic data is supported. This requires
+    pyWavelets along with Cython to use the C++ std:sort for efficiency.
+
+    REFERENCES
+      1. Wan Y and Shi D, 2007, "Joint exact histogram specification and image enhancement through
+         the wavelet transform", IEEE Transcations on Image Processing, 16(9):2245-2250.
+    """
+    # this uses pywt's 'symmetric' mode (duplicated edge)
+    from pywt import Wavelet
+    from ..util import get_dtype_max, as_unsigned
+    from .__wa import argsort # pylint: disable=no-name-in-module
+
+    # Normalize image data to 0 to 1
+    im = as_unsigned(im)
+    im = im / float(get_dtype_max(im.dtype))
+
+    # Compute the values needed to sort
+    thetas, tw0s = __compute_data(im, Wavelet(wavelet), nlevels)
+
+    # Perform the sorting
+    return argsort(im, thetas, tw0s, return_fails)
+
+def __compute_data(im, wavelet, nlevels=2):
+    """
+    Compute all of the subband coefficient data for the image using the given wavelet. This returns
+    two lists one with the sorted |theta_uij| values and the other with the theta_uij*w_uij_u > 0
+    values in the same order. The lists are indexed with j (the level) which gives an array of the
+    values at that level.
+
+    Each level still contains the approximate coefficients and thus this doesn't use pywt.wavedec2.
+    """
+    from numpy import abs # pylint: disable=redefined-builtin
+    filters, scales = __compute_filters(im.ndim, wavelet.filter_bank)
+
+    thetas = [] # |theta_uij| accessed like thetas[j][u*,i]
+    tw0s = []   # theta_uij*w_uij_u > 0 accessed like tw0s[j][u,i]
+    for _ in range(nlevels):
+        # Compute the coefficients
+        theta = __compute_coeffs(im, wavelet)
+        theta /= scales
+
+        # Compute the theta*w > 0 conditions
+        tw0 = __compute_positives(im.shape, theta, filters)
+
+        # The next subband level will use the current approximate result
+        im = theta[..., -1]
+
+        # Get the magnitude of the coefficients (except the approximate values)
+        abs(theta[..., :-1], theta[..., :-1])
+
+        # Sort the data
+        theta, tw0 = __sort_level(theta, tw0)
+
+        # Save
+        thetas.append(theta)
+        tw0s.append(tw0)
+
+    return thetas, tw0s
+
+def __compute_filters(ndim, filter_bank):
+    """
+    Compute the n-dimensional filters from the wavelet filter bank. The filter bank must start with
+    the 1D approximate and detail filters. The stack uses the first index for which filter, from
+    fully detail down to fully approximate. Also returns the scaling factor for each filter in an
+    array in the same order.
+    """
+    from functools import reduce
+    from numpy import stack, multiply, array
+    f_approx = filter_bank[0]
+    f_detail = filter_bank[1]
+    filters = stack([reduce(multiply.outer, f) for f in product((f_detail, f_approx), repeat=ndim)])
+    scales = array([max(-fltr.clip(None, 0).sum(), fltr.clip(0, None).sum()) for fltr in filters])
+    return filters, scales
+
+def __compute_coeffs(im, wavelet):
+    """
+    Compute the wavelet coefficients for an image (called theta_uij in the paper). Returns a stack
+    of values where the last index is which set of coefficients starting at fully detail down to
+    fully approximate.
+    """
+    from numpy import stack
+    from pywt import dwtn
+    theta = dwtn(im, wavelet)
+    return stack([theta[''.join(axes)] for axes in product('da', repeat=im.ndim)], axis=-1)
+
+def __compute_positives(shape, theta, filters):
+    """
+    Compute the positive indicators (called theta_uij*w_uij_u > 0 in the paper). Returns a stack of
+    values where the last index is which set of coefficients starting at fully detail down to fully
+    approximate.
+    """
+    from numpy import empty, greater, uint8
+    tw0 = empty(shape + (len(filters),), bool)
+    for offset in product((0, 1), repeat=len(shape)):
+        f_slcs = (slice(None),) + offset
+        tw_slcs = tuple(slice(x, None, 2) for x in offset)
+        greater(theta * filters[f_slcs], 0, tw0[tw_slcs])
+    return tw0.view(uint8)
+
+def __sort_level(theta, tw0):
+    """
+    Sort all of the data on a subband level. This sorts theta and then sorts tw0 in the same order
+    (but tw0 is twice as large in each dimension). Return the sorted theta and tw0.
+
+    NOTE: returned tw0 is actually the given tw0 (modified in-place), theta is a copy however.
+    """
+    from numpy import take_along_axis
+    order = theta.argsort(-1)
+    theta = take_along_axis(theta, order, -1)
+    for slc in product((slice(0, None, 2), slice(1, None, 2)), repeat=tw0.ndim-1):
+        tw0[slc] = take_along_axis(tw0[slc], order, -1)
+    return theta, tw0
